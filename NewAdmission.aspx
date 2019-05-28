eaf603895ffa2d2f01a39ec8bbd333a46a73e1e4
Confuse Coder <hakershsharma@gmail.com>
2019-05-28 20:38:23 +0530

    Add files via upload

---
+++ NewAdmission.aspx
@@ -0,0 +1,89 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="NewAdmission.aspx.cs" Inherits="WebApplication1.WebForm3" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style type="text/css">
+    .auto-style14 {
+        width: 184px;
+            height: 31px;
+        }
+    .auto-style15 {
+            margin-left: 80px;
+            height: 31px;
+        }
+        .auto-style19 {
+        height: 26px;
+    }
+    .auto-style20 {
+        height: 21px;
+    }
+    .auto-style21 {
+        width: 922px;
+    }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <table class="w-100">
+    <tr>
+        <td class="auto-style14">Enter School Name</td>
+        <td class="auto-style15" colspan="2">
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td class="auto-style19">Address</td>
+        <td colspan="2" class="auto-style19">
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td>StateName</td>
+        <td colspan="2">
+            <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" AutoPostBack="True" CssClass="mb-0">
+            </asp:DropDownList>
+        </td>
+    </tr>
+    <tr>
+        <td>City</td>
+        <td colspan="2">
+            <asp:DropDownList ID="DropDownList2" runat="server" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged">
+            </asp:DropDownList>
+        </td>
+    </tr>
+    <tr>
+        <td>Affiliated Board</td>
+        <td colspan="2">
+            <asp:DropDownList ID="DropDownList3" runat="server">
+            </asp:DropDownList>
+        </td>
+    </tr>
+    <tr>
+        <td class="auto-style20" colspan="3">Pin Code<asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td>Contact Number</td>
+        <td colspan="2">
+            <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td>Date Of Resignation</td>
+        <td colspan="2">
+            <asp:TextBox ID="TextBox8" runat="server" TextMode="Date"></asp:TextBox>
+        </td>
+    </tr>
+    <tr>
+        <td>Zonal</td>
+        <td colspan="2">
+            <asp:DropDownList ID="DropDownList4" runat="server">
+            </asp:DropDownList>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2" style="text-align:center" class="auto-style21">
+            <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+        </td>
+        <td style="text-align:center">
+            &nbsp;</td>
+    </tr>
+</table>
+</asp:Content>
---
+++ NewAdmission.aspx.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+
+        static string GetConneectionString()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Connection"].ToString();
+            return connectionString;
+        }
+        static SqlConnection GetConnection()
+        {
+            return new SqlConnection(GetConneectionString());
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack )
+            {
+                FillState();
+                AffiliatedBoard();
+                zonal();
+            }
+            
+            
+        }
+
+        void FillState()
+        {
+            using (SqlConnection con = new SqlConnection("data source=LAPTOP-FBF5J7NV;initial catalog=gic; integrated security=true"))
+            {
+                using (SqlCommand com = new SqlCommand("Select * from state", con))
+                {
+                    con.Open();
+                    SqlDataReader read = com.ExecuteReader();
+                    DropDownList1.DataSource = read;
+                    DropDownList1.DataTextField = "StateName";
+                    DropDownList1.DataValueField = "Sid";
+                    DropDownList1.DataBind();
+                }
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = GetConnection())
+            {
+                using (SqlCommand com = new SqlCommand())
+                {
+                    string sqlQuery = "insert into School(SchoolName,Address,CityId,StateName,AffiliatedBoard,PinCode,ContactNumber,DateOfRegistration,ZonalId) values(@SchoolName,@Address,@CityId,@StateName,@AffiliatedBoard,@PinCode,@ContactNumber,@DateOfResignation,@ZonalId)";
+                    com.Connection = con;
+                    com.CommandText = sqlQuery;
+                    com.Parameters.AddWithValue("@SchoolName", (TextBox1.Text));
+                    com.Parameters.AddWithValue("@Address", TextBox2.Text);
+                    com.Parameters.AddWithValue("@CityId", DropDownList2.SelectedValue);
+                    com.Parameters.AddWithValue("@stateName", DropDownList1.SelectedValue);
+                    com.Parameters.AddWithValue("@AffiliatedBoard", DropDownList3.SelectedValue);
+                    com.Parameters.AddWithValue("@PinCode", Convert.ToInt32(TextBox6.Text));
+                    com.Parameters.AddWithValue("@ContactNumber", TextBox7.Text);
+                    com.Parameters.AddWithValue("@DateOfResignation", TextBox8.Text);
+                    com.Parameters.AddWithValue("@ZonalId", DropDownList4.SelectedValue);
+                    con.Open();
+                    com.ExecuteNonQuery();
+                    con.Close();
+                    Response.Write("Record Inserted");
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FillCity();
+        }
+        void FillCity()
+        {
+            using (SqlConnection con = GetConnection())
+            {
+                using (SqlCommand com = new SqlCommand("Select * from city where sid=@sid", con))
+                {
+
+                    com.Parameters.AddWithValue("@sid", DropDownList1.SelectedValue);
+
+                    con.Open();
+                    SqlDataReader read = com.ExecuteReader();
+                    DropDownList2.DataSource = read;
+                    DropDownList2.DataTextField = "CityName";
+                    DropDownList2.DataValueField = "CityId";
+                    DropDownList2.DataBind();
+                    con.Close();
+
+
+                }
+            }
+        }
+        void AffiliatedBoard()
+        {
+            using (SqlConnection con = GetConnection())
+            {
+                using (SqlCommand com = new SqlCommand("Select * from AffiliatedBoard", con))
+                {
+
+
+                    con.Open();
+                    SqlDataReader read = com.ExecuteReader();
+                    DropDownList3.DataSource = read;
+                    DropDownList3.DataTextField = "BoardName";
+                    DropDownList3.DataValueField = "AffiliatedId";
+                    DropDownList3.DataBind();
+                    con.Close();
+
+
+                }
+            }
+        }
+
+        void zonal()
+        {
+            using (SqlConnection con = GetConnection())
+            {
+                using (SqlCommand com = new SqlCommand("Select * from Zonal", con))
+                {
+
+                    //com.Parameters.AddWithValue("@ZonalId", DropDownList4.SelectedValue);
+
+                    con.Open();
+                    SqlDataReader read = com.ExecuteReader();
+                    DropDownList4.DataSource = read;
+                    DropDownList4.DataTextField = "ZoneName";
+                    DropDownList4.DataValueField = "ZonalId";
+                    DropDownList4.DataBind();
+                    con.Close();
+
+
+                }
+            }
+        }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
+
+    
